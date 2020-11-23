ca1214c41481e1da012383ff570fe07a0ed645e9
Jessica Kelsall <hi@jesskelsall.dev>
2020-11-23 00:27:51 +0000

    Add dice

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,9 @@
 export const AVERAGE_TOLERANCE = 0.004
 
+export const EQUATION_DELIMITER = ' '
+
+// The notation for rolling one or more dice of the same type, used by D&D
+// e.g. d6, 2d8
+export const DICE_NOTATION_REGEX = /(\d*)\s*d\s*(\d+)/
+
 export const POLYHEDRAL_SIDES = [4, 6, 8, 10, 12, 20]
